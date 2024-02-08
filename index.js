6f493a89845e1d3c9092a13b33057604a2fb19a2
Douglas <douglas.souzag@hotmail.com>
2024-02-08 12:50:01 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', function(req, res) {
-    res.send('hello world');
-  });
+  res.send('Hello world!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
